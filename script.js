9df0173d962debb94a59e7e13d24bc8987186cb0
SD <harrypotter.starwars.shrish@gmail.com>
2020-07-15 20:34:00 +0530

    Added cleanup
     Changes to be committed:
    	new file:   .firebase/hosting..cache
    	modified:   script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 new ClipboardJS('.copy_button');
             
-function convert(){
+function convert() {
     document.getElementById("lang_content").innerHTML = "";
     var jsonText = document.getElementById("json_content").value;
-    console.log(jsonText);
     var parsed = JSON.parse(jsonText);
-    console.log(parsed);
     var langParsed = $('#lang_content');
     for (var k in parsed) {
-        var done = (k+"="+parsed[k]+"\n");
-        console.log(done);
+        var done = (k);
+        if(k.startsWith("block.")) {
+            done = done.replace("block.","tile.")
+        }
+        if (k.startsWith("block.") || k.startsWith("item.") || k.startsWith("entity.")){
+            done = (done.concat(".name"));
+        }
+        done = (done.concat("="+parsed[k]+"\n"));
         langParsed.append(done);
     }
     
